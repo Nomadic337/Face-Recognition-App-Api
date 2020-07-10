d23c1cb85cf75ef833c91c2e279023dcca6dcea9
Wolf S Laleau <devlaleau@gmail.com>
2020-07-10 10:04:46 -0400

    Working API

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile. handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', res.json("It works!!"))
+app.post('/imageurl', () => res.json("It works!!"))
 
 // (req, res) => { image.handleApiCall(req, res) }
 
