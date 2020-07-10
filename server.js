9eb07e7464782c5b19092c76b012b678d1f07caa
Wolf S Laleau <devlaleau@gmail.com>
2020-07-10 15:40:31 -0400

    API update

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => { res.send('It is working!!!') })        // working
 app.post('/signin', signin.handleSignin(db, bcrypt))        // working
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) // working
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) }) // working
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3000, () => {
