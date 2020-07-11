a5bf11a510d21876d46df792fbe27fb5ce2903dc
Wolf S Laleau <devlaleau@gmail.com>
2020-07-10 20:03:33 -0400

    API update

--- Controllers/image.js
+++ Controllers/image.js
@@ -7,18 +7,18 @@ const app = new Clarifai.App({
    });
 
 const handleApiCall = (req, res) => { 
-    // app.models
-    //     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    //     .then(data => {
-    //         res.json(data);
-    //     })
-    //     .catch(err => res.status(400).json('unable to work with API'))
     app.models
-        .predict('a403429f2ddf4b49b307e318f00e528b', req.body.input)
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
         .then(data => {
             res.json(data);
         })
-        .catch(err => res.status(400).json(err));
+        .catch(err => res.status(400).json('unable to work with API'))
+    // app.models
+    //     .predict('a403429f2ddf4b49b307e318f00e528b', req.body.input)
+    //     .then(data => {
+    //         res.json(data);
+    //     })
+    //     .catch(err => res.status(400).json(err));
 }
 
 // I can access server and database.
